8d4dc73a3759e4215ba6a0dd7dac83a82624d4a9
v-boy <visheshsingh16@gmail.com>
2019-06-16 10:58:50 -0500

    default imports and exports

---
+++ src/data.js
@@ -0,0 +1,9 @@
+const users = [
+  { name: 'yoshi', premium: true },
+  { name: 'mario', premium: false },
+  { name: 'peach', premium: true },
+  { name: 'luigi', premium: false },
+  { name: 'tuna', premium: true }
+];
+
+export default users;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { styleBody, addTitle } from './dom';
+import users from './data';
 
 console.log('index.js');
 styleBody();
 addTitle('hey boys!');
+
+console.log('Users: ', users);

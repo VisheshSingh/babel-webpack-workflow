315788264feb50c9cd0129cf992d495ecb11e799
v-boy <visheshsingh16@gmail.com>
2019-06-16 11:18:00 -0500

    build using webpack

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import users, { getPremUsers } from './data';
 console.log('index.js');
 styleBody();
 addTitle('hey boys!');
-
+console.log('test');
 console.log('Users: ', users);
 console.log('Premium Users: ', getPremUsers(users));

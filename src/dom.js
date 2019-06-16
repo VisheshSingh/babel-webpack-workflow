75568934da521d5f1fde3486a64dedf5a5066b71
v-boy <visheshsingh16@gmail.com>
2019-06-16 10:52:29 -0500

    Named Imports and Exports

--- src/dom.js
+++ src/dom.js
@@ -2,15 +2,12 @@ console.log('dom.js');
 
 const body = document.querySelector('body');
 
-const styleBody = () => {
+export const styleBody = () => {
   body.style.background = 'peachpuff';
 };
 
-const addTitle = text => {
+export const addTitle = text => {
   const title = document.createElement('h1');
   title.innerText = text;
   body.appendChild(title);
 };
-
-styleBody();
-addTitle('Hiyya ninjas!');
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import './dom';
+import { styleBody, addTitle } from './dom';
 
 console.log('index.js');
+styleBody();
+addTitle('hey boys!');
